622e14e66707d12cdb43d24674d19d648b751de8
keithrainz <61886785+keithrainz@users.noreply.github.com>
2021-01-14 13:27:19 +0200

    Update ConfigCover.js
    ADS

--- src/components/ConfigCover.js
+++ src/components/ConfigCover.js
@@ -216,7 +216,7 @@ class ConfigCover extends React.Component {
 	  <AdSense.Google
   client='ca-pub-2534224332415775'
   slot='7709604485'
-  style={{ width: 100%, height: 300, }}
+  style={{ width: 600, height: 300, }}
   format='auto'
 />
 					<div className="d-flex flex-row mt-2">
