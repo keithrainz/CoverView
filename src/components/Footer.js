e777dd234487cb435d4fd714163438b5d6deabf5
keithrainz <61886785+keithrainz@users.noreply.github.com>
2021-01-14 12:24:36 +0200

    Update Footer.js
    3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer>
 		
-            Made with &#10084; By <a href="https://keithrainz.me/blogcover/">Keith Rainz</a>
+            Made with &#10084; By <a href="https://keithrainz.me/blogcover/">Keith Rainz.</a>
         </footer>
     );
 
